e5507e0830e337ca036b591bb1f0665f4b5ccee3
MarcMG00 <marc.moli20@gmail.com>
2024-02-14 11:20:28 +0100

    First commit (added main)

--- src/pokemon/game/AppPokemon.java
+++ src/pokemon/game/AppPokemon.java
@@ -2,8 +2,9 @@ package pokemon.game;
 
 public class AppPokemon {
 
-	public AppPokemon() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
