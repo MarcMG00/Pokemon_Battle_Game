8cc2b8a5cab8ee3eb6621f5c15e942bc07895664
MarcMG00 <marc.moli20@gmail.com>
2024-05-01 16:15:38 +0200

    Added estados (burned, confused, paralyzed...)

---
+++ src/pokemon/enums/EstadosEnum.java
@@ -0,0 +1,22 @@
+package pokemon.enums;
+
+public enum EstadosEnum {
+	  // Estados efímeros
+	  CONFUSO,
+	  MALDITO,
+	  ENAMORADO,
+	  ATRAPADO,
+	  DRENADO,
+	  CANTO_MORTAL,
+	  // Estados persitentes
+	  PARALIZADO,
+	  ENVENENADO,
+	  GRAVEMENTE_ENVENENADO,
+	  CONGELADO,
+	  DORMIDO,
+	  QUEMADO,
+	  // No estado at the beginning
+	  SIN_ESTADO,
+	  // Pokemon is debilitaded
+	  DEBILITADO
+}
